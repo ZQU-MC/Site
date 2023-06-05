5e8b0ddfe50b0b2138fd51c63ad4279f39916fc9
9527 <2548635484@qq.com>
2023-06-06 00:15:42 +0800

    1

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/',
   lang: 'zh-CN',
   title: "ZQUMC 砚园工坊",
   titleTemplate: 'ZQUMC 砚园工坊',
